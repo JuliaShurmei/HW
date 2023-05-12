687c8b3e4244c19b8cb96caf447ff99d83d6c686
JuliaShurmei <ivanovaulia428@gmail.com>
2023-05-12 23:32:47 +0300

    Homework 2

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -4,11 +4,3 @@ interface Butt {
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
-export const Button = ({ value, className, onClick }: Butt) => {
-  return (
-    <button type='submit' className={className} onClick={onClick}>
-      {" "}
-      {value}{" "}
-    </button>
-  );
-};
